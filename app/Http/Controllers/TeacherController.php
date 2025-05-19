a731b2d1a876ae831c5408885394322bf2579d37
jefermar <jefermar17jjs@gmail.com>
2025-05-19 00:01:24 -0500

    index

--- app/Http/Controllers/TeacherController.php
+++ app/Http/Controllers/TeacherController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Teacher;
+use App\Models\Area;
+use App\Models\TrainingCenter;
 use Illuminate\Http\Request;
 
 class TeacherController extends Controller
 {
     //
+    public function index()
+    {
+        $teachers = Teacher::all();
+        $areas = Area::all();
+        $trainingCenters = TrainingCenter::all();
+        return view('teacher.index', compact('teachers','areas,trainingCenters'));
+    }
 }
+
