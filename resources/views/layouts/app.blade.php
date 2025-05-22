e0b3ae440059536b078697f56f6932663434b0b7
jefermar <jefermar17jjs@gmail.com>
2025-05-22 13:37:16 -0500

    Ultimo cambio

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,13 +9,13 @@
 </head>
 <body class="d-flex flex-column min-vh-100">
 
-    @include('includes.navbar') {{-- Tu navbar o header --}}
+    @include('includes.navbar') {{-- navbar o header --}}
 
     <main class="flex-fill container py-4">
-        @yield('content') {{-- Aquí se muestra el contenido de las vistas --}}
+        @yield('content') {{-- contenido de las vistas --}}
     </main>
 
-    @include('includes.footer') {{-- El footer --}}
+    @include('includes.footer') {{-- footer --}}
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
